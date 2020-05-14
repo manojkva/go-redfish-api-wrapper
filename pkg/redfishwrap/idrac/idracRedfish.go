ebc8115a97099b249cd9aab5e16f105488f1765f
Manoj Alva <manojkumar.alva@gmail.com>
2020-05-14 12:24:46 -0500

    Added REDFISH_SLEEPTIME_SECS env variable and code formatting

--- pkg/redfishwrap/idrac/idracRedfish.go
+++ pkg/redfishwrap/idrac/idracRedfish.go
@@ -3,13 +3,21 @@ package idrac
 import (
 	"context"
 	"fmt"
-	//"os"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	RFWrap "github.com/manojkva/go-redfish-api-wrapper/pkg/redfishwrap"
 	redfish "opendev.org/airship/go-redfish/client"
 )
+
+var RedfishSleepTimeSeconds int = 0
+
+func init() {
+	RedfishSleepTimeSeconds, _ = strconv.Atoi(os.Getenv("REDFISH_SLEEPTIME_SECS"))
+
+}
 
 type IdracRedfishClient struct {
 	Username  string
@@ -53,10 +61,10 @@ func (a *IdracRedfishClient) CheckJobStatus(jobId string) bool {
 	start := time.Now()
 	var result bool = false
 
-       if  jobId == ""{
-           fmt.Println("Job ID is null. Returing Failed Job Status")
-           return false
-       }
+	if jobId == "" {
+		fmt.Println("Job ID is null. Returing Failed Job Status")
+		return false
+	}
 
 	for {
 
@@ -69,30 +77,30 @@ func (a *IdracRedfishClient) CheckJobStatus(jobId string) bool {
 
 		} else {
 			fmt.Printf("Failed to check the status")
-		    return false
+			return false
 		}
 
 		if timeelapsedInMinutes >= 5 {
 			fmt.Println("\n- FAIL: Timeout of 5 minute has been hit, update job should of already been marked completed. Check the iDRAC job queue and LC logs to debug the issue")
-			return true
-		} else if jobInfo.Messages != nil {
-		if strings.Contains(jobInfo.Messages[0].Message, "failed") {
-			fmt.Println("FAIL")
 			return false
+		} else if jobInfo.Messages != nil {
+			if strings.Contains(jobInfo.Messages[0].Message, "Failed") {
+				fmt.Println("FAIL")
+				return false
 
-		} else if strings.Contains(jobInfo.Messages[0].Message, "scheduled") {
-			//	fmt.Prinln("\n- PASS, job ID %s successfully marked as scheduled, powering on or rebooting the server to apply the update" % data[u"Id"] ")
-			result = true
-			break
+			} else if strings.Contains(jobInfo.Messages[0].Message, "scheduled") {
+				//	fmt.Prinln("\n- PASS, job ID %s successfully marked as scheduled, powering on or rebooting the server to apply the update" % data[u"Id"] ")
+				result = true
+				break
 
-		} else if strings.Contains(jobInfo.Messages[0].Message, "completed successfully") {
-			//		fmt.Prinln("\n- PASS, job ID %s successfully marked as scheduled, powering on or rebooting the server to apply the update" % data[u"Id"] ")
-			fmt.Println("Success")
-			result = true
-			break
-		}
-		}else {
-			time.Sleep(time.Second*5)
+			} else if strings.Contains(jobInfo.Messages[0].Message, "completed successfully") {
+				//		fmt.Prinln("\n- PASS, job ID %s successfully marked as scheduled, powering on or rebooting the server to apply the update" % data[u"Id"] ")
+				fmt.Println("Success")
+				result = true
+				break
+			}
+		} else {
+			time.Sleep(time.Second * 5)
 			continue
 		}
 	}
@@ -104,7 +112,7 @@ func (a *IdracRedfishClient) RebootServer(systemID string) bool {
 	ctx := a.createContext()
 
 	//Systems/System.Embedded.1/Actions/ComputerSystem.Reset
-	resetRequestBody := redfish.ResetRequestBody { ResetType: redfish.RESETTYPE_FORCE_RESTART }
+	resetRequestBody := redfish.ResetRequestBody{ResetType: redfish.RESETTYPE_FORCE_RESTART}
 
 	return RFWrap.ResetServer(ctx, a.HostIP, systemID, resetRequestBody)
 
@@ -112,7 +120,7 @@ func (a *IdracRedfishClient) RebootServer(systemID string) bool {
 
 func (a *IdracRedfishClient) PowerOn(systemID string) bool {
 	ctx := a.createContext()
-	resetRequestBody := redfish.ResetRequestBody { ResetType: redfish.RESETTYPE_ON }
+	resetRequestBody := redfish.ResetRequestBody{ResetType: redfish.RESETTYPE_ON}
 
 	return RFWrap.ResetServer(ctx, a.HostIP, systemID, resetRequestBody)
 
@@ -120,7 +128,7 @@ func (a *IdracRedfishClient) PowerOn(systemID string) bool {
 
 func (a *IdracRedfishClient) PowerOff(systemID string) bool {
 	ctx := a.createContext()
-	resetRequestBody := redfish.ResetRequestBody { ResetType: redfish.RESETTYPE_GRACEFUL_SHUTDOWN }
+	resetRequestBody := redfish.ResetRequestBody{ResetType: redfish.RESETTYPE_GRACEFUL_SHUTDOWN}
 
 	return RFWrap.ResetServer(ctx, a.HostIP, systemID, resetRequestBody)
 }
@@ -158,20 +166,20 @@ func (a *IdracRedfishClient) InsertISO(managerID string, mediaID string, imageUR
 
 }
 
-func (a *IdracRedfishClient) GetVirtualDisks(systemID string, controllerID string)[]string {
+func (a *IdracRedfishClient) GetVirtualDisks(systemID string, controllerID string) []string {
 
 	ctx := a.createContext()
 	idrefs := RFWrap.GetVolumes(ctx, a.HostIP, systemID, controllerID)
-	if idrefs == nil{
+	if idrefs == nil {
 		return nil
 	}
 	virtualDisks := []string{}
-	for _,id := range idrefs{
+	for _, id := range idrefs {
 
 		fmt.Printf("VirtualDisk Info %v\n", id.OdataId)
-		vd := strings.Split(id.OdataId,"/")
+		vd := strings.Split(id.OdataId, "/")
 		if vd != nil {
-		  virtualDisks = append(virtualDisks,vd[len(vd)-1])
+			virtualDisks = append(virtualDisks, vd[len(vd)-1])
 		}
 	}
 	return virtualDisks
@@ -190,7 +198,7 @@ func (a *IdracRedfishClient) CreateVirtualDisk(systemID string, controllerID str
 	drives := []redfish.IdRef{}
 
 	for _, uri := range urilist {
-		driveinfo := fmt.Sprintf("/redfish/v1/Systems/%s/Storage/Drives/%s",systemID, uri)
+		driveinfo := fmt.Sprintf("/redfish/v1/Systems/%s/Storage/Drives/%s", systemID, uri)
 		drives = append(drives, redfish.IdRef{OdataId: driveinfo})
 	}
 
@@ -203,7 +211,7 @@ func (a *IdracRedfishClient) CreateVirtualDisk(systemID string, controllerID str
 	return RFWrap.CreateVirtualDisk(ctx, a.HostIP, systemID, controllerID, createvirtualBodyReq)
 }
 
-func (a *IdracRedfishClient)CleanVirtualDisksIfAny(systemID string, controllerID string) bool{
+func (a *IdracRedfishClient) CleanVirtualDisksIfAny(systemID string, controllerID string) bool {
 
 	var result bool = false
 
@@ -212,52 +220,52 @@ func (a *IdracRedfishClient)CleanVirtualDisksIfAny(systemID string, controllerID
 	totalvirtualDisks := len(virtualDisks)
 	var countofVDcreated int = 0
 	// for testing skip the OS Disk
-	//virtualDisks = virtualDisks[1:] 
+	//virtualDisks = virtualDisks[1:]
 	if totalvirtualDisks == 0 {
 		fmt.Printf("No existing RAID disks found")
 		result = true
 	} else {
-		for _,vd  := range virtualDisks {
-			jobid  := a.DeletVirtualDisk(systemID,vd)
-			fmt.Printf("Delete Job ID %v\n",jobid)
+		for _, vd := range virtualDisks {
+			jobid := a.DeletVirtualDisk(systemID, vd)
+			fmt.Printf("Delete Job ID %v\n", jobid)
 			result = a.CheckJobStatus(jobid)
 
 			if result == false {
-				fmt.Printf("Failed to delete virtual disk %v\n",vd)
+				fmt.Printf("Failed to delete virtual disk %v\n", vd)
 				return result
 			}
-                        time.Sleep(time.Second*10) //Sleep in between calls
+			time.Sleep(time.Second * time.Duration(RedfishSleepTimeSeconds)) //Sleep in between calls
 			countofVDcreated += 1
 
 		}
 	}
-	if countofVDcreated  != totalvirtualDisks {
-		result  = false
+	if countofVDcreated != totalvirtualDisks {
+		result = false
 	}
 
-    return result
+	return result
 }
 
-func (a * IdracRedfishClient)GetNodeUUID(systemID string )(string, bool){
+func (a *IdracRedfishClient) GetNodeUUID(systemID string) (string, bool) {
 
 	ctx := a.createContext()
-	computerSystem, _  := RFWrap.GetSystem(ctx,a.HostIP,systemID)
+	computerSystem, _ := RFWrap.GetSystem(ctx, a.HostIP, systemID)
 
-	if computerSystem != nil{
+	if computerSystem != nil {
 		return computerSystem.UUID, true
 	}
 	return "", false
 }
 
-func (a * IdracRedfishClient)GetPowerStatus(systemID string ) bool{
+func (a *IdracRedfishClient) GetPowerStatus(systemID string) bool {
 
 	ctx := a.createContext()
-	computerSystem, _  := RFWrap.GetSystem(ctx,a.HostIP,systemID)
+	computerSystem, _ := RFWrap.GetSystem(ctx, a.HostIP, systemID)
 
-	if computerSystem != nil{
-                if  computerSystem.PowerState  ==  "On"{
-                    return  true
-               }
+	if computerSystem != nil {
+		if computerSystem.PowerState == "On" {
+			return true
+		}
 	}
 	return false
 }
